b26df66ba4151a71804dfe84595541718de90f86
o_weerasinghe <oshanweerasinghe28@gmail.com>
2021-09-20 18:50:16 +0530

    stabelizedCommitmentBackend1.1V

--- models/Feedbacks.js
+++ models/Feedbacks.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const feedbackSchema = new Schema({
+    name : {
+        type : String,
+        required : true
+    },
     stat : {
         type : String,
         required : true
--- routes/FeedbackRoute.js
+++ routes/FeedbackRoute.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const feedback = require("../models/Feedbacks");
 let Feedback  = require("../models/Feedbacks");
 const { json } = require("express");
 
@@ -7,13 +6,15 @@ const { json } = require("express");
 
 router.route("/addFeedback").post((req,res)=>{
 
-    const stat = req.body.name;
-    const feedback = req.body.age;
+    const name = req.body.name;
+    const stat = req.body.stat;
+    const feedback = req.body.feedback;
 
     const newFeedback = new Feedback({
+        name,
         stat,
         feedback
-    })
+    });
 
     newFeedback.save().then(()=>{
         res.json("Feedback Added")
@@ -30,41 +31,6 @@ router.route("/").get((req,res)=>{
         res.json(feedback)
     }).catch((err)=>{
         console.log(err)
-    })
-})
-
-
-/////////////////////////////////////////////////Update////////////////////////////////////////////////////////////
-
-router.route("/updateFeedback/:id").put(async(req, res)=>{
-    let FeedID = req.params.id;
-    const {stat, feedback} = req.body;
-
-    const updateFeedback = {
-        stat,
-        feedback
-    }
-
-    const update = await Feedback.findByIdAndUpdate(FeedID, updateFeedback).then(()=>{
-        res.status(200).send({status: "Feedback updated", feedback: update})
-    }).catch((err)=>{
-        console.log(err);
-        res.status(500).send({status: "Error with updating data"});
-    })   
-})
-
-///////////////////////////////////////////////////////Delete///////////////////////////////////////////////////////////////////
-
-
-router.route("/deleteFeedback/:id").delete(async(req, res) => {
-    let FeedID = req.params.id;
-
-    await Complaint.findByIdAndDelete(FeedID)
-    .then(() =>{
-        res.status(200).send({status: "Feedback deleted"});
-    }).catch((err)=>{
-        console.log(err.message);
-        res.status(500).send({status: "Eror with delete feedback", error: err.message})
     });
 });
 
